473541ed636de25c9bfeea41f8c1a1f167703b00
SabaPangani <77338263+SabaPangani@users.noreply.github.com>
2022-12-17 17:59:37 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    const apiURL = 'https://api.adviceslip.com/advice'
+
+    $(".dice").click(function(){
+        $.ajax({
+            type: "GET",
+            url: apiURL,
+            dataType: "json",
+            success: function (response) {
+                var numberID = Number(response.slip.id)
+                var adviceText = response.slip.advice
+                $(".numberText").text(`Advice #${numberID}`)
+                $(".adviceText").text(adviceText)
+                console.log(numberID,adviceText)
+            }
+        });
+    })
+});
